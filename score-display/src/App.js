ad70882891bc9627d450379638965a4d758e780c
Leila <nb.leila10@gmail.com>
2019-04-12 22:16:35 -0500

    Add Display and Dashboard to app and test to be sure it renderes without crashing

--- score-display/src/App.js
+++ score-display/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react"
 
 import "./App.css"
+import Display from "./components/display/Display.js"
+import Dashboard from "./components/dashboard/Dashboard.js"
 
 class App extends Component {
+  state = {
+    ballsCount: 0,
+    strikesCount: 0
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <h1>Welcome to webtesting-ii</h1>
         </header>
+        <main>
+          <Display
+            ballsCount={this.state.ballsCount}
+            strikesCount={this.state.strikesCount}
+          />
+        </main>
       </div>
     )
   }
